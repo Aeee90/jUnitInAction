c84c8c1c655dbbe150ba7d562855e27175ccea51
seoyeon <sylee@tanagement.net>
2022-01-12 20:34:37 +0900

    init

---
+++ src/main/kotlin/aeee/api/junitInAction/JunitInActionApplication.kt
@@ -0,0 +1,11 @@
+package aeee.api.junitInAction
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JunitInActionApplication
+
+fun main(args: Array<String>) {
+	runApplication<JunitInActionApplication>(*args)
+}
---
+++ src/test/kotlin/aeee/api/junitInAction/JunitInActionApplicationTests.kt
@@ -0,0 +1,13 @@
+package aeee.api.junitInAction
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JunitInActionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
